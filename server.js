70f342ebd3c997706b02e777d3f96590d70f20b7
MarceloGomesFollow <marcelojg@gmail.com>
2025-08-30 16:50:18 -0300

    v2 - smoke test

--- server.js
+++ server.js
@@ -4,3 +4,4 @@ const port = process.env.PORT || 8080;
 
 app.get("/", (_req, res) => res.send("SGQ Arfrio QA - App Hosting OK 🚀"));
 app.listen(port, () => console.log(`Listening on ${port}`));
+res.send("SGQ Arfrio QA - v2 ✅");
